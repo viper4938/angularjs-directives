ab05dae55297a813effd5877f5ab923eecfaac0c
viper4938 <viper4938@aol.com>
2015-02-24 20:20:17 -0700

    Refactor
    Refactor to not have hard coded value for form properties count (based on logic all angular form properties begin with $). Add uniqueId to simplify parsing the dynamically added form controls. Support non $scope first level form properties ($scope.Object.property)

--- ng-repeat.js
+++ ng-repeat.js
@@ -8,9 +8,26 @@ icisNg.directive('ngRepeat', function () {
                     return keyItem.substring(0, 4) == 'form';
                 });
                 
-                var destroyMe = scope.$watch(function () { return Object.keys(scope[formKeysArray]).length; }, function (val) {                    
-                    //There are 11 basic built in form properties and methods so we only care if actual form elements are added (a count above 11)
-                    if (val > 11) {
+                var destroyMe = scope.$watch(function () { return Object.keys(scope[formKeysArray]).length; }, function (val) {                                                                             
+                    //Built in angular form properties begin with $ including our $processFormRows and $cleanupRows so we only care if actual form elements are added                    
+                    if (val > Object.keys(ctrl).filter(function (i) { return i.substring(0, 1) == "$"; }).length) {
+                        //Use the index to find item in ng-repeat's array and set it equal to scope.$id using regex to pase attrs.ng-repeat
+                        var expression = attrs.ngRepeat;
+                        var match = expression.match(/^\s*([\s\S]+?)\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?\s*$/);
+                        var sourceList = match[2];
+                        var splitSourceList = sourceList.split(".");
+                        var pointer = scope.$parent.$parent;
+                        //Work with nested properties such as Object.Property
+                        for (var i = 0; i < splitSourceList.length; i++) {
+                            if (pointer[splitSourceList[i]]) {
+                                pointer = pointer[splitSourceList[i]];
+                            }                                          
+                        }
+                        //Assign the uniqueId of the form control to the source array item so it can be tracked
+                        if (pointer[scope.$parent.$index]) {
+                            pointer[scope.$parent.$index]["uniqueId"] = scope.$id;                            
+                        }
+
                         for (var i = 0; i < formKeysArray.length; i++) {
                             var propertyKeysArray = Object.keys(scope[formKeysArray[i]]).filter(function (keyItem) {
                                 return keyItem.substring(0, 1) != '$' && keyItem != 'this';
